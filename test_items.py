5b4c0232787e0d9636a2ff33729a4c580d291524
WasAlexHere <sethtorston@mail.ru>
2020-07-30 10:25:04 +0300

    added test test_items.py for testing different support of different languages

---
+++ test_items.py
@@ -0,0 +1,9 @@
+import time
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_verify_add_to_basket_button(browser):
+    browser.get(link)
+    time.sleep(10)
+    assert browser.find_element_by_xpath("//*[@id='add_to_basket_form']/button"), "There is no Add to Basket button!"
